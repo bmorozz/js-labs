0532e078bae8715742afe3277aea5abf555a50d9
bmorozz <bohdan.moroz.20@pnu.edu.ua>
2026-03-23 15:41:00 +0200

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -7,7 +7,9 @@
       console.log(speakWord + " " + name);
     }
 
-    return { speak: speak };
+    return {
+      speak: speak
+    };
   })();
 
   var byeSpeaker = (function () {
@@ -17,18 +19,32 @@
       console.log(speakWord + " " + name);
     }
 
-    return { speak: speak };
+    return {
+      speak: speak
+    };
   })();
 
   var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i = 0; i < names.length; i++) {
+
     var firstLetter = names[i].charAt(0).toLowerCase();
 
     if (firstLetter === "j") {
       byeSpeaker.speak(names[i]);
     } else {
       helloSpeaker.speak(names[i]);
+    }
+
+  }
+
+  console.log("=== Custom logic: names ending with 'a' ===");
+
+  for (var i = 0; i < names.length; i++) {
+    var lastLetter = names[i].charAt(names[i].length - 1).toLowerCase();
+
+    if (lastLetter === "a") {
+      console.log("Special Hello " + names[i]);
     }
   }
 
