e7d663f8a59675c6fb521f24f9792348bf4f41bc
bmorozz <bohdan.moroz.20@pnu.edu.ua>
2026-03-23 15:29:17 +0200

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,1 +1,35 @@
+(function () {
 
+  var helloSpeaker = (function () {
+    var speakWord = "Hello";
+
+    function speak(name) {
+      console.log(speakWord + " " + name);
+    }
+
+    return { speak: speak };
+  })();
+
+  var byeSpeaker = (function () {
+    var speakWord = "Good Bye";
+
+    function speak(name) {
+      console.log(speakWord + " " + name);
+    }
+
+    return { speak: speak };
+  })();
+
+  var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})();
